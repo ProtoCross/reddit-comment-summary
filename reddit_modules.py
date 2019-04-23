5a72011a0be132d5208c24ab296a103071b3dd23
Josh Ryal <S525501@mail.nwmissouri.edu>
2019-04-23 16:16:19 -0500

    Updated to allow selection of number of sentences in summary

--- reddit_modules.py
+++ reddit_modules.py
@@ -5,12 +5,12 @@ Created on Fri Apr 19 19:48:29 2019
 @author: Josh
 """
 
-import praw
 import nltk
-import pickle
 import numpy
 from nltk.tokenize import sent_tokenize
 from praw.models import MoreComments
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 def scrape_comments(reddit, submission):
     sentences = []
@@ -24,9 +24,11 @@ def scrape_comments(reddit, submission):
 
 def process_comments(sentences):
     words = list()
+    #Tokenize all of the words present
     for sent in sentences:
         words.extend(map(lambda x: x.lower(), nltk.tokenize.word_tokenize(sent)))
     
+    #Set stopwords to find end of sentences
     stopwords = set(nltk.corpus.stopwords.words('english'))
     for punct in ",.'?:;’“”":
         stopwords.add(punct)
@@ -36,11 +38,13 @@ def process_comments(sentences):
     
     sorted_terms = sorted(freqDist.items(), key=lambda x: x[1], reverse=True)
     
+    #Find the 12 most common words
     n_most_common = [word[0] for word in sorted_terms if word[0] not in stopwords][:N]
     
     summaries = summarize(sentences, n_most_common, 5, 8)
     
-    return summaries['top-n']
+    #put 'mean-score' or 'top-n'
+    return summaries['mean-score']
 
 
 def cluster_score(cluster):
@@ -54,40 +58,42 @@ def score_sentences(sentences, important_words, CLUSTER_THRESH=5):
     
     for sent in map(nltk.tokenize.word_tokenize, sentences):
         word_idx = []
-        # Find the positions of all the important words in the
-        # sentence
+        
+        #Find the positions of all the important words in a sentence
         for word in important_words:
             if word in sent:
                 word_idx.append(sent.index(word))
         word_idx.sort()
-        # Build the clusters based on the cluster threshold
+        
+        #Build clusters based on threshold
         if len(word_idx) > 0:
             clusters = []
             current_cluster = [word_idx[0]]
             for idx in word_idx[1:]:
-                # check to see if the current index is close
-                # enough to the previous index
+                #Check to see if the current index is close to previous index
                 if idx - word_idx[-1] < CLUSTER_THRESH:
                     current_cluster.append(idx)
+                    
+                #If it's not, start a new cluster
                 else:
-                    # if it's not, start a new cluster
                     clusters.append(current_cluster)
                     current_cluster = [idx]
             clusters.append(current_cluster)
             
-            # Score clusters, keep track of the largest cluster score
-            # for the sentence
+            #Score clusters, keep track of the largest cluster score
             scores.append(max(map(cluster_score, clusters)))
-        else: # a sentence with no important words has a score of 0
+        #If no important words are present, score 0
+        else:
             scores.append(0)
     return scores
  
 def summarize(sentences, important_words, CTHRESH=5, TOP_SENTENCES=5):
+    N = 5
     scores = score_sentences(sentences, important_words, CTHRESH)
     avg = numpy.mean(scores)
     std_dev = numpy.std(scores)
     score_threshold = avg + 0.5 * std_dev
-    mean_scored = [t[0] for t in enumerate(scores) if t[1] > score_threshold]
+    mean_scored = [t[0] for t in enumerate(scores) if t[1] > score_threshold][:N]
     
     sorted_scores = sorted(enumerate(scores), key=lambda x: x[1], reverse=True)[:TOP_SENTENCES]
     sorted_indexes = sorted([s[0] for s in sorted_scores])
@@ -95,3 +101,11 @@ def summarize(sentences, important_words, CTHRESH=5, TOP_SENTENCES=5):
     return {'top-n': ' '.join([sentences[i] for i in sorted_indexes]),
             'mean-score': ' '.join([sentences[i] for i in mean_scored])}
     
+def generate_cloud(summary):
+    #Pass summary to wordcloud for generation
+    wordcloud = WordCloud(max_font_size = 40).generate(summary)
+    plt.figure()
+    plt.imshow(wordcloud, interpolation = 'bilinear')
+    plt.axis('off')
+    plt.show()
+    return wordcloud
