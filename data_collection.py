500c0071a294926f2e03a26bca4d83c8f6589c0e
Josh Ryal <S525501@mail.nwmissouri.edu>
2019-04-23 16:16:55 -0500

    Added word cloud printing

--- data_collection.py
+++ data_collection.py
@@ -9,12 +9,15 @@ import praw
 import reddit_modules
 from info import ID, SECRET
 from nltk.tokenize import sent_tokenize
+import pickle
 
+#Create reddit instance
 reddit = praw.Reddit(client_id=ID,
                      client_secret=SECRET,
                      user_agent='web_mining')
-submission = reddit.submission(id='apeqa2')
+submission = reddit.submission(id='bg6q6s')
 
+#Store content of comments
 sentences = reddit_modules.scrape_comments(reddit, submission)
     
 #for f in sentences:
@@ -22,3 +25,4 @@ sentences = reddit_modules.scrape_comments(reddit, submission)
     
 summary = reddit_modules.process_comments(sentences)
 print(summary)
+reddit_modules.generate_cloud(summary)
